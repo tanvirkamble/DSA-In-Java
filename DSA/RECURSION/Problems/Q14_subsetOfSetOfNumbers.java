a0d8756d9951e5d4d776fa15952e7dab89fd92dc
tanvir kamble <tanukamble22@gmail.com>
2024-09-01 17:37:49 +0530

    Q14

---
+++ DSA/RECURSION/Problems/Q14_subsetOfSetOfNumbers.java
@@ -0,0 +1,43 @@
+package DSA.RECURSION.Problems;
+
+import java.util.ArrayList;
+
+public class Q14_subsetOfSetOfNumbers {
+
+    
+    // Static counter to keep track of subset index
+    private static int subsetIndex = 0;
+    
+    public static void printSubets(ArrayList<Integer> subset) {
+        System.out.println("Subset no. " + subsetIndex);
+        if (subset.isEmpty()) {
+            System.out.println("empty subset");
+        } else {
+            for (int i = 0; i < subset.size(); i++) {
+                System.out.print(subset.get(i) + " ");
+            }
+            System.out.println();
+        }
+        subsetIndex++; 
+    }
+
+    public static void findSubset(int n, ArrayList<Integer> subset ){
+       if(n==0){
+           printSubets(subset);
+        return;
+       }
+
+       //add n
+       subset.add(n);
+       findSubset(n-1, subset);
+       
+       //remove n
+       subset.remove(subset.size()-1);
+       findSubset(n-1, subset);
+    }
+    public static void main(String[] args) {
+        int n=3;
+        ArrayList<Integer> subset = new ArrayList<>();
+        findSubset(n, subset);
+    }
+}
