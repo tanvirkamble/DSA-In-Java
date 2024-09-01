26837a501ed11f1bb73005e084815b1a24d996d0
tanvir kamble <tanukamble22@gmail.com>
2024-09-01 17:12:13 +0530

    Q13

---
+++ DSA/RECURSION/Problems/Q13_singleOrPaied.java
@@ -0,0 +1,20 @@
+package DSA.RECURSION.Problems;
+
+public class Q13_singleOrPaied {
+
+    public static int callGuest(int n){
+        if (n<=1) {
+            return 1;
+        }
+        //single
+        int way1 = callGuest(n-1);
+        // pairs
+        int way2 = (n-1)*callGuest(n-2);
+        return way1 + way2;
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(callGuest(n));
+    }
+    
+}
