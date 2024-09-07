d94d3030324c4ab7d29a7dfdfc6e79f088f8dab2
tanvir kamble <tanukamble22@gmail.com>
2024-09-07 20:22:00 +0530

    stack using aray list

--- DSA/STACK/stack2.java
+++ DSA/STACK/stack2.java
@@ -1,60 +1,45 @@
 package DSA.STACK;
 
+import java.util.*;
 
 public class stack2 {
-   Node head;
-   int size;
-
-   class Node {
-    int data;
-    Node next;
-
-    Node(int x){
-        this.data = x;
-        this.next = null;
-    }
-}
-    boolean isempty(){
-          if (head == null) {
-            return true;
-          }
-          return false;
-    }
-    void push(int x){
-        Node tobeadded = new Node(x);
-        if (isempty()) {
-            head = tobeadded;
-            return ;
+   
+    static class stack {
+    
+        
+        static ArrayList<Integer> list = new ArrayList<>();
+        boolean isempty(){
+            return list.size() == 0;
         }
-        tobeadded.next = head;
-        head = tobeadded;
-        return;
-    }
-    int pop(){
-        if (isempty()) {
-            return -1;
+        void push(int data){
+            list.add(data);
         }
-        int top = head.data;
-        head = head.next;
-        return top;
-    }
-    int peek(){
-        if (isempty()) {
-            return -1;
+        int pop(){
+            if (isempty()) {
+                return -1;
+            }
+            int top = list.get(list.size()-1);
+            list.remove(top);
+            return top;
         }
-        return head.data;
+        int peek(){
+            if (isempty()) {
+                return -1;
+            }
+            return list.get(list.size()-1);
+        }
+        
     }
-
-
    public static void main (String args[]){
-      stack2 s = new stack2();
-      s.push(1);
-      s.push(2);
-      s.push(3);
-      s.push(4);
-      while (!s.isempty()) {
-            System.out.println(s.peek());
-            s.pop();
-      }
+     stack s = new stack();
+     s.push(0);
+     s.push(1);
+     s.push(2);
+     s.push(3);
+
+     while (!s.isempty()) {
+        System.out.println(s.peek());// prints top of stack
+        s.pop(); // pops the stack moves to next element
+     }
    }
 }
