d3d3404dedc720bb0724f3f0db569733b659dcc0
tanvir kamble <tanukamble22@gmail.com>
2024-09-07 19:56:29 +0530

    stack basic push,pop,peek

---
+++ DSA/STACK/stack1.java
@@ -0,0 +1,69 @@
+package DSA.STACK;
+
+class stack1 {
+    static Node head;
+    int size;
+    
+    class Node {
+       int data;
+       Node next;
+
+       //constructor
+       Node(int x){
+        this.data = x;
+        this.next = null;
+       }
+    }
+   
+  
+    boolean isempty(){
+        return head == null;
+    }
+    void push(int x){
+        Node tobePushed = new Node(x);
+        if (isempty()) {
+            head = tobePushed;
+            return ;
+        }
+        
+      
+        tobePushed.next = head;
+        head = tobePushed;
+        return;
+         
+    }
+    
+    int pop(){
+        if (isempty()) {
+            return -1;
+        }
+        int top = head.data;
+        head = head.next;
+
+        return top ;
+
+    }
+    int peek(){
+        if (isempty()) {
+            return -1;
+        }
+        return head.data;
+    }
+    
+
+    public static void main (String args[]){
+          stack1 s = new stack1();
+          s.push(0);
+          s.push(1);
+          s.push(2);
+        //  s.printStack(); 
+        //  s.printStack(); 
+        
+            while (!s.isempty()) {
+                System.out.println(s.peek());
+                s.pop();
+            }
+
+          s.pop();         
+    }
+}
