0f28bc755c3e4194146ab73d44403b90f2716297
tanvir kamble <tanukamble22@gmail.com>
2024-09-10 21:12:23 +0530

    sum of all node

--- DSA/TREE/tree3.java
+++ DSA/TREE/tree3.java
@@ -1,11 +1,10 @@
 package TREE;
 
-public class tree2 {
+public class tree3 {
     static class Node{
         int data;
         Node left;
         Node right;
-
         Node(int x){
             this.data = x;
             this.left = null;
@@ -16,7 +15,7 @@ public class tree2 {
         static int i = -1;
         static Node buildTree(int x[]){
             i++;
-            // Base case: if the current element is -1, return null (no node)
+
             if (x[i] == -1) {
                 return null;
             }
@@ -25,25 +24,22 @@ public class tree2 {
             newNode.right = buildTree(x);
 
             return newNode;
-
         }
     }
-    static int countOfNodes(Node x){
-        // Base case: if the node is null, return 0 (no node)
-        if (x == null) {
+
+    static int sumOfNOdes(Node n){
+        if (n == null) {
             return 0;
         }
-        // Recursively count the nodes in the left and right subtrees
-        int leftNodes = countOfNodes(x.left);
-        int rightNodes = countOfNodes(x.right);
-
-        return leftNodes + rightNodes + 1;
+        int leftNodeSum = sumOfNOdes(n.left);
+        int rightNodeSum =sumOfNOdes(n.right) ;
+        return leftNodeSum + rightNodeSum + n.data ;
     }
-    public static void main(String[] args) {
-        
-        int  nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
-        Node root = binaryTree.buildTree(nodes);
-        System.out.println("Total number of nodes in the tree: " + countOfNodes(root));  // Output the node count
 
+    public static void main(String[] args) {
+        int  nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+        Node root = binaryTree.buildTree(nodes);
+        System.out.println(sumOfNOdes(root));
     }
 }
