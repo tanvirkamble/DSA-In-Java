be9fcd44605095a4d5490a762a4af592e383fa35
tanvir kamble <tanukamble22@gmail.com>
2024-09-23 12:43:28 +0530

    print asubarray of sum if present

--- Questions/Q7.java
+++ Questions/Q7.java
@@ -1,23 +1,77 @@
 package Questions;
+import java.util.*;
+
 
 public class Q7 {
-    public static void main(String[] args) {
-        int [] a ={1,2,3,-3,1,1,1,4,2-3} ;
-        int k = 3;//sum of subarrays to be found
-        int count = 0;// count of subarrays exist
+    public static void count(int[] a, int sum){
+        int count = 0;
+        int checksum;
 
-        //O(n) = x^3
         for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                int sum = 0;
-                for (k = i; k <= j; k++) {
-                    sum = sum + a[k];
-                }
-                if (k == sum) {
-                    count++;
+            checksum = a[i];
+            if (checksum == sum) {
+                count++; 
+                System.out.println("sum at postion " + i);               
+            }
+            else {
+                for(int j=i+1; j < a.length ; j++){
+                    checksum = checksum + a[j];
+                    if (checksum == sum) {
+                        count++;
+                        System.out.println("from "+ i + " to " + j);
+                    }
                 }
             }
         }
         System.out.println(count);
     }
-}
+
+   public static ArrayList<Integer> subarraySum(int[] a, int n, int sum){
+        ArrayList<Integer> x = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            int checksum = a[i];
+            if (checksum == sum) {
+                x.add(i);
+                return x;
+            }  else {
+                for (int j = i+1; j < n; j++) {
+                    checksum = checksum + a[j];
+                    if (checksum == sum) {
+                        x.add(i);
+                        x.add(j);
+                        return x;
+                    }
+                }
+            }   
+        }
+        x.add(-1);
+        
+        return x;       
+   }
+
+    public static void main(String[] args)
+    {
+        // int[] arr = { 15, 2, 4, 8, 9, 5, 10, 23 };
+        // int[] arr = {1, 4, 0, 0, 3, 10, 5};
+        // int[] arr = {1, 4};
+        // int[] arr = { 23 };
+        int[] arr = { 1,2,3,-3,1,1,1,4,2,-3};
+
+
+        int n = arr.length;
+
+        // int sum = 23;
+        // int sum = 7;
+        // int sum = 2;
+        // int sum = 23;
+        int sum = 3;
+        ArrayList<Integer> result
+            = subarraySum(arr, n, sum);
+        for (int i : result)
+            System.out.print("position : "+i + " ");
+        System.out.println();
+        count(arr, sum);
+    }
+    }
+
