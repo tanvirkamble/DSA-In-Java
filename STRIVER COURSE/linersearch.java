4ded8af614809fa26eb8336f4de0182de6577e76
tanvir kamble <tanukamble22@gmail.com>
2025-01-14 01:09:34 +0530

    commit

---
+++ STRIVER COURSE/linersearch.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class linersearch {
+        public static int linearSearch(int n, int num, int []arr){
+        // Write your code here.
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == num){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
