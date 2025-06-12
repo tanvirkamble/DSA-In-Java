152fe3c6dd2e9ea34b84a187754d6a2af65399c3
tanvir kamble <tanukamble22@gmail.com>
2025-06-12 15:41:44 +0530

    conversion fo arr to LL traversal of linked list

--- STRIVER COURSE/LinkedList.java
+++ STRIVER COURSE/LinkedList.java
@@ -13,15 +13,35 @@ class Node {
 }
 
 public class LinkedList {
+    private static Node arrayToLL(int arr[]){
+        if (arr.length == 0) return null;
+        Node head = new Node(arr[0]);
+        Node current = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            current.next = new Node(arr[i]);
+            current = current.next;
+        }
+        return head;
+    }
      public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5};
         Node x = new Node(arr[3]);
         Node y = new Node(arr[2], x);
 
-        System.out.println(x);
-        System.out.println(x.data);
-        System.out.println(y);
-        System.out.println(y.data);
+        // System.out.println(x);
+        // System.out.println(x.data);
+        // System.out.println(y);
+        // System.out.println(y.data);
+
+        Node head = arrayToLL(arr);
+        // System.out.println(head.data);
+
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
 
      }
-}
+} 
