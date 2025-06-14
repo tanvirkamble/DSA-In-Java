57feac1645219fe4ed797103708cbb0f59823373
tanvir kamble <tanukamble22@gmail.com>
2025-06-14 15:10:49 +0530

    insert before and after of (head & tail)

--- STRIVER COURSE/DoublyLinkedList.java
+++ STRIVER COURSE/DoublyLinkedList.java
@@ -40,8 +40,19 @@ public class DoublyLinkedList {
 }
 
 public static Node DeleteHeadOfDLL(Node head){
+    Node deletedNode = head;
+
+    String prevData = (deletedNode.prev != null) ? String.valueOf(deletedNode.prev.data) : "null";
+    String nextData = (deletedNode.next != null) ? String.valueOf(deletedNode.next.data) : "null";
+
+    System.out.println("Deleted node: " + deletedNode.data +" (prev: " + prevData + ", next: " + nextData + ")");
+
+    nextData = null;
+
     head = head.next;
     if (head != null) head.prev = null;  // Important line!
+
+
 
     return head;
 }
@@ -50,6 +61,15 @@ public static Node DeleteTailOfDLL(Node head){
     while(temp.next.next != null){
         temp = temp.next;
     }
+    Node deletedNode = temp.next;
+
+    String prevData = (deletedNode.prev != null) ? String.valueOf(deletedNode.prev.data) : "null";
+    String nextData = (deletedNode.next != null) ? String.valueOf(deletedNode.next.data) : "null";
+
+    System.out.println("Deleted node: " + deletedNode.data + " (prev: " + prevData + ", next: " + nextData + ")");
+    prevData = null;
+    System.out.println("Deleted node: " + deletedNode.data + " (prev: " + prevData + ", next: " + nextData + ")");
+
     temp.next = null;
     
     return head;
@@ -87,6 +107,59 @@ public static Node deleteAtKthPosition(Node head, int k){
     return head;
 }
 
+private static Node insertBeforeHead(Node head, int e ){
+    Node temp = head;
+    Node newNode = new Node(e);
+    
+    temp.prev = newNode;
+    newNode.next = temp;
+    head = newNode;
+    
+    
+    return head;
+}
+private static Node insertAfterHead(Node head, int e ){
+    Node temp = head;
+    Node newNode = new Node(e);
+
+    Node afterHead = head.next;
+    head.next = newNode;
+    newNode.prev = head;
+    newNode.next = afterHead;
+    afterHead.prev = newNode;
+
+    return head;
+}
+
+private static Node insertBeforeTail(Node head, int e){    
+    Node temp = head;
+    Node newNode = new Node(e);
+    while(temp.next.next != null){
+        temp = temp.next;
+    }
+    Node tail = temp.next;
+    temp.next = newNode;
+    newNode.prev = temp;
+    newNode.next = tail;
+    tail.prev = newNode;
+
+        
+    return head;
+}
+private static Node insertAfterTail(Node head, int e){    
+    Node temp = head;
+    Node newNode = new Node(e);
+
+    while(temp.next != null){
+        temp = temp.next;
+    }
+    Node tail = temp;
+    tail.next = newNode;
+    newNode.prev = tail;
+
+    return head;
+}
+
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6};
         Node head = array2DLL(arr);
@@ -96,8 +169,18 @@ public static Node deleteAtKthPosition(Node head, int k){
         // printLL(h);
         // Node h = DeleteTailOfDLL(head);
         // printLL(h);
-        Node h = deleteAtKthPosition(head,5);
-        printLL(h);
+        // Node h = deleteAtKthPosition(head,0);
+        // printLL(h);// 0 indexed
+        
+        
+        // Node h = insertBeforeHead(head,100);
+        // printLL(h);
+        // Node h = insertAfterHead(head,100);
+        // printLL(h);
+        // Node h = insertBeforeTail(head,100);
+        // printLL(h);
+        // Node h = insertAfterTail(head,100);
+        // printLL(h);
 
     }
 }
