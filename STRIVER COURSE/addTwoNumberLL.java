9fe24696bf62172f220bb4f3edf8a75bf0069030
tanvir kamble <tanukamble22@gmail.com>
2025-06-15 16:38:37 +0530

    another approach to add number

--- STRIVER COURSE/addTwoNumberLL.java
+++ STRIVER COURSE/addTwoNumberLL.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class addTwoNumberLL {
     public static Node addTwoNumbers(Node l1, Node l2) {
 
@@ -61,6 +63,52 @@ public class addTwoNumberLL {
 
     }
 
+     public static Node addTwoNumbers2(Node l1, Node l2) {
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
+
+        Node temp1 = l1;
+        Node temp2 = l2;
+
+        Node newHead = new Node(0);
+        Node temp3 = newHead;
+
+        while(temp1 != null){
+            arr1.add(temp1.data);
+            temp1 = temp1.next;
+        }
+        while(temp2 != null){
+            arr2.add(temp2.data);
+            temp2 = temp2.next;
+        }
+
+        int size = Math.max(arr1.size() , arr2.size()) ;
+        int a[] = new int [size + 1];
+
+        for(int i = 0 ; i < size ; i++){
+            int x = 0, y = 0;
+            if(i < arr1.size()) x = arr1.get(i);
+            else x = 0;
+            if(i < arr2.size()) y = arr2.get(i);
+            else y = 0;
+
+            int sum = x+y+a[i];
+            a[i+1] = sum / 10;
+            sum = sum % 10; 
+
+            temp3.next = new Node(sum);
+
+            temp3 = temp3.next;
+        }
+
+        if (a[size] != 0) {
+            temp3.next = new Node(a[size]);
+        }
+
+        return newHead.next;
+
+    }
+
     public static void main(String[] args) {
          Node l1 = new Node(2);
     l1.next = new Node(4);
@@ -73,11 +121,16 @@ public class addTwoNumberLL {
 
     // Add the two numbers
     Node result = addTwoNumbers(l1, l2);
+    Node result2 = addTwoNumbers2(l1, l2);
 
     // Print the result
     while(result != null) {
         System.out.print(result.data + " ");
         result = result.next;
+    }
+    while(result2 != null) {
+        System.out.print(result2.data + " ");
+        result2 = result2.next;
     }
     }
 
